348814d492f9f1a71b5a1a0d10191cc3edd633f1
marckes04 <maresp95@gmail.com>
2022-08-04 00:48:06 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Greeting from './components/pure/greeting';
+import Greetingf from './components/pure/greetingF';
+import TaskListComponent from './components/container/task_list';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {/* <Greetingf name="Mario"> </Greetingf>*/}
+      {/*Componente de listado de tareas*/}
+      <TaskListComponent></TaskListComponent>
       </header>
     </div>
   );
